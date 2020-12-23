fdb14d557319ec46a08755dfe670f95a3d5ed9d1
Erhan Bilgin <bilginerhan95@gmail.com>
2020-12-23 15:49:59 +0300

    Initial Commit

---
+++ SwiftUIExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIExample
+//
+//  Created by Erhan Bilgin on 23.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIExample/SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleApp.swift
+//  SwiftUIExample
+//
+//  Created by Erhan Bilgin on 23.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
