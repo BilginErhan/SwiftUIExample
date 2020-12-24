8b6603e604d2733076de570e34f2f2a6d99c102b
Erhan Bilgin <ebilgin@invio.com>
2020-12-24 12:32:49 +0300

    Slot View from example

---
+++ SwiftUIExample/UIExample/SwiftUISlots/SwiftUISlotView.swift
@@ -0,0 +1,111 @@
+//
+//  SwiftUISlotView.swift
+//  SwiftUIExample
+//
+//  Created by Erhan Bilgin on 24.12.2020.
+//
+
+import SwiftUI
+
+struct SwiftUISlotView: View {
+    
+    @State private var credits = 1000
+    @State private var numbers = [1, 2, 0]
+    private var symbols = ["apple", "star", "cherry"]
+    private var betAmount = 10
+    
+    var body: some View {
+        ZStack{
+            Rectangle()
+                .foregroundColor(Color(red: 200/255, green: 143/255, blue: 32/255
+                ))
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            Rectangle()
+                .foregroundColor(Color(red: 228/255, green: 195/255, blue: 76/255))
+                .rotationEffect(Angle(degrees: 45))
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack{
+                Spacer()
+                //Title
+                HStack{
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                    
+                    Text("SwiftUI Slots")
+                        .bold()
+                        .foregroundColor(.white)
+                    
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                }.scaleEffect(2)
+                
+                Spacer()
+                //Credit counter
+                Text("Credits: \(String(credits))")
+                    .foregroundColor(.black)
+                    .padding(.all,10)
+                    .background(Color.white.opacity(0.5))
+                    .cornerRadius(20)
+                
+                Spacer()
+                //Cards
+                HStack{
+                    Spacer()
+                    Image(symbols[numbers[0]])
+                        .resizable()
+                        .aspectRatio(1,contentMode: .fit)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                    
+                    Image(symbols[numbers[1]])
+                        .resizable()
+                        .aspectRatio(1,contentMode: .fit)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                    
+                    Image(symbols[numbers[2]])
+                        .resizable()
+                        .aspectRatio(1,contentMode: .fit)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                    
+                    Spacer()
+                }
+                Spacer()
+                Button(action: {
+                    self.credits += 1
+                    
+                    self.numbers[0] = Int.random(in: 0...symbols.count - 1)
+                    
+                    self.numbers[1] = Int.random(in: 0...symbols.count - 1)
+                    
+                    self.numbers[2] = Int.random(in: 0...symbols.count - 1)
+                    
+                    if self.numbers[0] == self.numbers[1] &&
+                        self.numbers[1] == self.numbers[2] {
+                        self.credits += self.betAmount * 10
+                    }else{
+                        self.credits -= self.betAmount
+                    }
+                }, label: {
+                    Text("Spin")
+                        .bold()
+                        .foregroundColor(.white)
+                        .padding(.all, 10)
+                        .padding([.leading, .trailing], 30)
+                        .background(Color.pink)
+                        .cornerRadius(20)
+                })
+                Spacer()
+            }
+        }
+    }
+}
+
+struct SwiftUISlotView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUISlotView()
+    }
+}
